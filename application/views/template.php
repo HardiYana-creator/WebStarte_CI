a97578e7b5fa774d8f48ea74a2efb1197000d0d7
HardiYana-Creator <hardiyana23@yahoo.co.id>
2020-12-30 20:47:11 -0800

    Edit template

--- application/views/template.php
+++ application/views/template.php
@@ -7,7 +7,7 @@
 	<!-- Required meta tags -->
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-	<title>Syndash - Bootstrap4 Admin Template</title>
+	<title>Web Starter</title>
 	<!--favicon-->
 	<link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
 	<!-- Vector CSS -->
